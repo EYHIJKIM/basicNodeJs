810e6bc250e960f37c5d064d7eb645ef7a102a49
JiHye Kim <crk33333@gmail.com>
2021-08-16 15:50:21 +0900

    Number,String

---
+++ syntax/Number.js
@@ -0,0 +1,4 @@
+console.log("1+1 :" + 1+1);
+console.log("1*2 :" +1*2);
+console.log("10/3 :" +10/3);
+console.log("10%3 :" +10%3);
---
+++ syntax/String.js
@@ -0,0 +1,8 @@
+//결합 연산자
+console.log('1'+'1');
+
+console.log(
+'I received weekly supervision from a psychologist at my community-counseling training site. She was smart and perceptive,with decades of experience helping addicts; I was lucky to have her guidance. Three months into treatment,I told my supervisor Grace was doing so well that we had agreed to cut our sessions from weekly to biweekly.remarkable how quickly she’s improving, I said. But my supervisor was cautious. Getting clean is hard, she told me, but staying clean is harder.'
+.length
+
+);
