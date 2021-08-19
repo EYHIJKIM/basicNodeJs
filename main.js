c33b37ee1fa9c7cd22ead5691eace12879c04070
JiHye Kim <crk33333@gmail.com>
2021-08-19 21:07:09 +0900

    다이나믹 웹 생성(파일내용->본문)

--- main.js
+++ main.js
@@ -16,40 +16,44 @@ var app = http.createServer(function(request,response){
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
+
     /*
     fs.readFileSync(경로) :
     node.js가 해당 경로의 파일을 읽음
     response.end('응답')
     node.js가 해당 괄호의 소스코드로 응답
     */
+
     response.writeHead(200);
 
-    var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?Id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-        <img src="coding.jpg" width="100%">
-        </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-        </p>
-      </body>
-      </html>
+    //페이지가 열릴 때마다 직접 파일을 읽어서 로드하고 있으므로,
+    //해당 파일 내용이 수정되어도 서버를 껐다 킬 필요가 없음!!(실시간으로 읽어서 띄우기 떄문..)
+    fs.readFile(`data/${title}`,'utf8',function(err, description){
+        var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>
+              ${description}
+            </p>
+          </body>
+          </html>
+        `
+        response.end(template);
+    });
 
 
-    `
-
-    response.end(template);
 
 });
 app.listen(3000);
